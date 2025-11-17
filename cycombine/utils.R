fb705646d0be318624c10eeb285a5c25c9f9dd72
Isabella Garcia <i4garcia@ucsd.edu>
2025-11-16 21:52:02 -0800

    TEMP FIX resolve filename for reading utility

--- cycombine/utils.R
+++ cycombine/utils.R
@@ -7,7 +7,7 @@ suppressPackageStartupMessages({
 dir_create <- function(x) if (!dir.exists(x)) dir.create(x, recursive = TRUE)
 
 load_fcs_txt <- function(path) {
-    files <- list.files(path, pattern="\\.fcs\\.txt$", full.names = TRUE)
+    files <- list.files(path, pattern="\\.txt$", full.names = TRUE)
     rbindlist(lapply(files, function(f) {
                          dt <- fread(f, sep="\t")
                          dt[, filename := basename(f)]
